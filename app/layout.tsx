7dec089486a2d7ab80cf2aa4da39f88c9f7851ca
jeddelich@gmail.com <jeddelich@gmail.com>
2026-04-22 18:36:56 -0400

    style: choose and integrate typeface

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import Navbar from "@/components/layout/Navbar";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -26,9 +21,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className={`${geistSans.className} min-h-full flex flex-col`}>
         <Navbar />
         <main className="flex-1">{children}</main>
       </body>
